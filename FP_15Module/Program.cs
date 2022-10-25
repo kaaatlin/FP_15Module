f43bbab9f7937c9103acc28687375791d0519032
admin <admin@DESKTOP-NKFQO7K>
2022-10-25 21:47:42 +0300

    Добавьте файлы проекта.

---
+++ FP_15Module/Program.cs
@@ -0,0 +1,29 @@
+﻿// Напишите метод, который соберет всех учеников всех классов в один список, используя LINQ.
+namespace FP_15Module
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var classes = new[]
+            {
+               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
+               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
+               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
+            };
+            var allStudents = GetAllStudents(classes);
+
+            Console.WriteLine(string.Join("\n", allStudents));
+        }
+        static string[] GetAllStudents(Classroom[] classes)
+        {
+            var joined = classes.SelectMany(x => x.Students).ToArray();
+            return joined;
+        }
+
+        public class Classroom
+        {
+            public List<string> Students = new List<string>();
+        }
+    }
+}
